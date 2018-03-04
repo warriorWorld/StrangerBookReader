9500f117e85f9a417e225d978621dd1602baec19
warriorWorld <772192594@qq.com>
2018-03-04 22:27:08 +0800

    epub topbar

--- app/src/main/java/com/warrior/hangsu/administrator/strangerbookreader/business/epub/EpubActivity.java
+++ app/src/main/java/com/warrior/hangsu/administrator/strangerbookreader/business/epub/EpubActivity.java
@@ -1,24 +1,43 @@
 package com.warrior.hangsu.administrator.strangerbookreader.business.epub;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.ClipboardManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AbsListView;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
 import com.warrior.hangsu.administrator.strangerbookreader.R;
+import com.warrior.hangsu.administrator.strangerbookreader.bean.YoudaoResponse;
+import com.warrior.hangsu.administrator.strangerbookreader.business.read.NewReadActivity;
+import com.warrior.hangsu.administrator.strangerbookreader.configure.Globle;
+import com.warrior.hangsu.administrator.strangerbookreader.configure.ShareKeys;
+import com.warrior.hangsu.administrator.strangerbookreader.db.DbAdapter;
 import com.warrior.hangsu.administrator.strangerbookreader.listener.TextSelectionListener;
 import com.warrior.hangsu.administrator.strangerbookreader.manager.SettingManager;
 import com.warrior.hangsu.administrator.strangerbookreader.utils.BaseActivity;
+import com.warrior.hangsu.administrator.strangerbookreader.utils.SharedPreferencesUtils;
+import com.warrior.hangsu.administrator.strangerbookreader.utils.ToastUtil;
 import com.warrior.hangsu.administrator.strangerbookreader.utils.ToastUtils;
+import com.warrior.hangsu.administrator.strangerbookreader.volley.VolleyCallBack;
+import com.warrior.hangsu.administrator.strangerbookreader.volley.VolleyTool;
+import com.warrior.hangsu.administrator.strangerbookreader.widget.dialog.MangaDialog;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import nl.siegmann.epublib.domain.Book;
@@ -37,6 +56,17 @@ public class EpubActivity extends BaseActivity {
     private TranslateWebView epubWebView;
     private Book book;
     private boolean is_show_bottom = false;
+    private DbAdapter db;//数据库
+    private ClipboardManager clip;//复制文本用
+    private MangaDialog dialog;
+    private TextView topBarRight, topBarLeft;
+    private String bookTitle;
+    /**
+     * 时间
+     */
+    private SimpleDateFormat sdf;
+    private Date curDate;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +74,11 @@ public class EpubActivity extends BaseActivity {
         //取消状态栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        db = new DbAdapter(this);
+        clip = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        sdf = new SimpleDateFormat("HH:mm");
+        curDate = new Date(System.currentTimeMillis());//获取当前时间
+        date = sdf.format(curDate);
 
         Intent intent = getIntent();
         bookPath = intent.getStringExtra("bookPath");
@@ -53,7 +88,7 @@ public class EpubActivity extends BaseActivity {
         initUI();
         initEpubLib();
         loadChapter();
-        ToastUtils.showSingleToast("epub格式仅支持长按翻译");
+        ToastUtils.showLongToast("epub格式仅支持长按翻译\nepub是上下翻页的\n可以用音量键跳转章节。");
         recoverProgress();
     }
 
@@ -73,32 +108,105 @@ public class EpubActivity extends BaseActivity {
                         epubWebView.clearFocus();
                     }
                 }, 150);//n秒后执行Runnable中的run方法
-//                translation(word);
-                ToastUtils.showSingleToast(word);
+                translation(word);
             }
         });
         epubWebView.setOnCustomScroolChangeListener(new TranslateWebView.ScrollInterface() {
             @Override
             public void onSChanged(int l, int t, int oldl, int oldt) {
-                //WebView的总高度
-                float webViewContentHeight = epubWebView.getContentHeight() * epubWebView.getScale();
-                //WebView的现高度
-                float webViewCurrentHeight = (epubWebView.getHeight() + epubWebView.getScrollY());
-                System.out.println("webViewContentHeight=" + webViewContentHeight);
-                System.out.println("webViewCurrentHeight=" + webViewCurrentHeight);
-                if ((webViewContentHeight - webViewCurrentHeight) < 50 && !is_show_bottom) {
-                    System.out.println("WebView滑动到了底端");
-                    ToastUtils.showSingleToast("可以用音量键翻到下一章");
-                    is_show_bottom = true;
+                if (oldt - t > 50) {
+                    refreshTime();
                 }
+                isWebBottom();
             }
         });
+        topBarLeft = (TextView) findViewById(R.id.top_bar_left);
+        topBarRight = (TextView) findViewById(R.id.top_bar_right);
+
+        int separatorPosition = bookPath.lastIndexOf(File.separator);
+        int dotPosition = bookPath.lastIndexOf(".");
+        bookTitle = bookPath.substring(separatorPosition + 1, dotPosition);
+        topBarLeft.setText(bookTitle);
+        topBarRight.setText("第" + (currentChapter + 1) + "章" + "  " + date);
+
         hideBaseTopBar();
     }
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_webview;
+    }
+
+    private void refreshTime() {
+        curDate.setTime(System.currentTimeMillis());//获取当前时间
+        date = sdf.format(curDate);
+        topBarRight.setText("第" + (currentChapter + 1) + "章" + "  " + date);
+    }
+
+    private void isWebBottom() {
+        //WebView的总高度
+        float webViewContentHeight = epubWebView.getContentHeight() * epubWebView.getScale();
+        //WebView的现高度
+        float webViewCurrentHeight = (epubWebView.getHeight() + epubWebView.getScrollY());
+        System.out.println("webViewContentHeight=" + webViewContentHeight);
+        System.out.println("webViewCurrentHeight=" + webViewCurrentHeight);
+        if ((webViewContentHeight - webViewCurrentHeight) < 50 && !is_show_bottom) {
+            System.out.println("WebView滑动到了底端");
+            ToastUtils.showSingleToast("可以用音量键翻到下一章");
+            is_show_bottom = true;
+        }
+    }
+
+    private void translation(final String word) {
+        clip.setText(word);
+        //记录查过的单词
+        db.insertWordsBookTb(word);
+        if (SharedPreferencesUtils.getBooleanSharedPreferencesData
+                (this, ShareKeys.CLOSE_TRANSLATE, false)) {
+            return;
+        }
+        String url = Globle.YOUDAO + word;
+        HashMap<String, String> params = new HashMap<String, String>();
+        VolleyCallBack<YoudaoResponse> callback = new VolleyCallBack<YoudaoResponse>() {
+
+            @Override
+            public void loadSucceed(YoudaoResponse result) {
+                if (null != result && result.getErrorCode() == 0) {
+                    YoudaoResponse.BasicBean item = result.getBasic();
+                    String t = "";
+                    if (null != item) {
+                        for (int i = 0; i < item.getExplains().size(); i++) {
+                            t = t + item.getExplains().get(i) + ";";
+                        }
+                        showOnlyOkDialog(word, result.getQuery() + " [" + item.getPhonetic() +
+                                "]: " + "\n" + t);
+                    } else {
+                        ToastUtil.tipShort(EpubActivity.this, "没查到该词" + word);
+                    }
+                } else {
+                    ToastUtil.tipShort(EpubActivity.this, "网络连接失败");
+                }
+            }
+
+            @Override
+            public void loadFailed(VolleyError error) {
+                ToastUtil.tipShort(EpubActivity.this, "error" + error);
+            }
+        };
+        VolleyTool.getInstance(this).requestData(Request.Method.GET,
+                EpubActivity.this, url, params,
+                YoudaoResponse.class, callback);
+
+    }
+
+    private void showOnlyOkDialog(String title, String msg) {
+        if (null == dialog) {
+            dialog = new MangaDialog(this);
+        }
+        dialog.show();
+        dialog.setTitle(title);
+        dialog.setMessage(msg);
+        dialog.setOkText("确定");
     }
 
     private void initEpubLib() {
@@ -142,8 +250,8 @@ public class EpubActivity extends BaseActivity {
 
     private void recoverProgress() {
         int pos[] = SettingManager.getInstance().getReadProgress(bookPath);
-        currentChapter=pos[0];
-        int currentProgress=pos[1];
+        currentChapter = pos[0];
+        int currentProgress = pos[1];
         loadChapter();
         epubWebView.setScrollY(currentProgress);
     }
@@ -152,6 +260,7 @@ public class EpubActivity extends BaseActivity {
     protected void onDestroy() {
         super.onDestroy();
         saveProgress();
+        db.closeDb();
     }
 
     @Override
@@ -179,6 +288,8 @@ public class EpubActivity extends BaseActivity {
             String data = new String(book.getContents().get(currentChapter).getData());
 
             epubWebView.loadDataWithBaseURL(baseUrl, data, "text/html", "UTF-8", null);
+
+          refreshTime();
         } catch (Exception e) {
             e.printStackTrace();
         }
