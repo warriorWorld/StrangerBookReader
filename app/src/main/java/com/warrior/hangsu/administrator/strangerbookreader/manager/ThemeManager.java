8f6c21d2aa329d56cdb9eb28bb472c18ae5ae0ac
suhang <suhang@roobo.com>
2020-07-23 18:31:42 +0800

    theme

--- app/src/main/java/com/warrior/hangsu/administrator/strangerbookreader/manager/ThemeManager.java
+++ app/src/main/java/com/warrior/hangsu/administrator/strangerbookreader/manager/ThemeManager.java
@@ -40,32 +40,8 @@ public class ThemeManager {
     public static final int LEATHER = 3;
     public static final int GRAY = 4;
     public static final int NIGHT = 5;
-    public static final String[] THEME_LIST = {"默认", "黄色", "绿色", "羊皮纸", "灰色"};
-
-    public static void setReaderTheme(int theme, View view) {
-        switch (theme) {
-            case NORMAL:
-                view.setBackgroundResource(R.drawable.theme_white_bg);
-                break;
-            case YELLOW:
-                view.setBackgroundResource(R.drawable.theme_yellow_bg);
-                break;
-            case GREEN:
-                view.setBackgroundResource(R.drawable.theme_green_bg);
-                break;
-            case LEATHER:
-                view.setBackgroundResource(R.drawable.theme_leather_bg);
-                break;
-            case GRAY:
-                view.setBackgroundResource(R.drawable.theme_gray_bg);
-                break;
-            case NIGHT:
-                view.setBackgroundResource(R.drawable.theme_night_bg);
-                break;
-            default:
-                break;
-        }
-    }
+    public static final int WHITE = 6;
+    public static final String[] THEME_LIST = {"默认", "黄色", "绿色", "羊皮纸", "灰色", "黑色", "白色"};
 
     public static Bitmap getThemeDrawable(int theme) {
         Bitmap bmp = Bitmap.createBitmap(ScreenUtils.getScreenWidth(), ScreenUtils.getScreenHeight(), Bitmap.Config.ARGB_8888);
@@ -88,22 +64,11 @@ public class ThemeManager {
             case NIGHT:
                 bmp.eraseColor(ContextCompat.getColor(AppUtils.getAppContext(), R.color.read_theme_night));
                 break;
+            case WHITE:
+                bmp.eraseColor(ContextCompat.getColor(AppUtils.getAppContext(), R.color.white));
             default:
                 break;
         }
         return bmp;
     }
-
-    public static List<ReadTheme> getReaderThemeData(int curTheme) {
-        int[] themes = {NORMAL, YELLOW, GREEN, LEATHER, GRAY, NIGHT};
-        List<ReadTheme> list = new ArrayList<>();
-        ReadTheme theme;
-        for (int i = 0; i < themes.length; i++) {
-            theme = new ReadTheme();
-            theme.theme = themes[i];
-            list.add(theme);
-        }
-        return list;
-    }
-
 }
